f5cbe2b81d1c4abc50eadfbb90c7368ae4a743c5
GeisianHReis <geisian.hreis@gmail.com>
2022-01-15 23:32:13 -0300

    Botoẽs de navegação

--- frontend/src/componentes/MovieCards/index.tsx
+++ frontend/src/componentes/MovieCards/index.tsx
@@ -1,4 +1,5 @@
 import MovieScore from "componentes/MovieScore";
+import { Link } from "react-router-dom";
 function MovieCards() {
     const movie = {
         id: 1,
@@ -7,16 +8,19 @@ function MovieCards() {
         count: 2,
         score: 4.5
     };
-   return(
+    return (
         <div>
-        <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-        <div className="dsmovie-card-bottom-container">
-            <h3>{movie.title}</h3>
-            <MovieScore />
-            <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <MovieScore />
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
+
+            </div>
         </div>
-    </div>
-   );
+    );
 }
 
 export default MovieCards;
