62a5e860a407c6a9183a625a14af50cb1580e305
GeisianHReis <geisian.hreis@gmail.com>
2022-01-17 02:36:43 -0300

    Show score

--- frontend/src/componentes/MovieStars/index.tsx
+++ frontend/src/componentes/MovieStars/index.tsx
@@ -1,20 +1,65 @@
-import {ReactComponent as EstrelaCheia} from "assets/img/estrela-cheia.svg";
-import {ReactComponent as EstrelaMetade} from "assets/img/estrela-metade.svg";
-import {ReactComponent as EstrelaVazia} from "assets/img/estrela-vazia.svg";
+import { ReactComponent as EstrelaCheia } from "assets/img/estrela-cheia.svg";
+import { ReactComponent as EstrelaMetade } from "assets/img/estrela-metade.svg";
+import { ReactComponent as EstrelaVazia } from "assets/img/estrela-vazia.svg";
 import "./styles.css";
 
-function MovieStars() {
+type Props = {
+  score: number;
+}
+type StarProps = {
+  fill: number;
+}
 
-    return (
 
-        <div className="dsmovie-stars-container">
-            <EstrelaCheia />
-            <EstrelaCheia />
-            <EstrelaCheia />
-            <EstrelaMetade />
-            <EstrelaVazia />
-        </div>
-    );
+// EX:
+// getFills(3.5) => [1, 1, 1, 0.5, 0]
+// getFills(4.1) => [1, 1, 1, 1, 0.5]
+function getFills(score: number) {
+
+  const fills = [0, 0, 0, 0, 0];
+
+  const integerPart = Math.floor(score);
+
+  for (let i = 0; i < integerPart; i++) {
+    fills[i] = 1;
+  }
+
+  const diff = score - integerPart;
+  if (diff > 0) {
+    fills[integerPart] = 0.5;
+  }
+
+  return fills;
+}
+
+function Star({ fill } : StarProps) {
+  if (fill === 0) {
+    return <EstrelaVazia />
+  }
+  else if (fill === 1) {
+    return <EstrelaCheia />
+  }
+  else {
+    return <EstrelaMetade />
+  }
+}
+
+
+
+function MovieStars({ score }: Props) {
+
+  const fills = getFills(score);
+
+  return (
+
+    <div className="dsmovie-stars-container">
+      <Star fill={fills[0]} />
+      <Star fill={fills[1]} />
+      <Star fill={fills[2]} />
+      <Star fill={fills[3]} />
+      <Star fill={fills[4]} />
+    </div>
+  );
 }
 
 export default MovieStars;
